cfed0a932bd0dffcd87a4d5e40569ec7b6cf8d15
SunilTmb <sunilpb22@gmail.com>
2022-11-29 16:01:13 +0530

    components add

--- contact/src/components/App.js
+++ contact/src/components/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import AddContact from './AddContact.js';
+import ContactList from './ContactList.js';
 
 function App() {
   return (
-    <div>Hello</div>
-    
+   <div>
+    <Header/>
+    <ContactList/>
+    <AddContact/>
+   </div>
   );
 }
 
